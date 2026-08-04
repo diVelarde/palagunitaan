954eab77011500136ede2e2516b9b7cf8e2cd837
diVelarde <divelarde@gbox.adnu.edu.ph>
2026-08-05 05:16:22 +0800

    SETUP-004 configure MySQL database

--- server/index.js
+++ server/index.js
@@ -32,3 +32,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const db = require('./config/db');
+
+db.query('SELECT 1')
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
