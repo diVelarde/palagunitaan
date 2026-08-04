fb5f13395fb82e40b1190d1ec4df1bfd12d2cab3
diVelarde <divelarde@gbox.adnu.edu.ph>
2026-08-05 05:57:55 +0800

    SETUP-005 connect frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import api from './api/axios';
+
+console.log('API URL:', process.env.REACT_APP_API_URL);
 
 function App() {
+  useEffect(() => {
+
+    api.get('/api/test')
+      .then(res => console.log(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-600">
-      <h1 className="text-4xl font-bold text-white">
-        Palagunitaan Setup Complete
-      </h1>
+    <div>
+      <h1>Palagunitaan</h1>
     </div>
   );
 }
