67633c0c9d0c6eeafd44733bbdb06cbec7b047a4
diVelarde <divelarde@gbox.adnu.edu.ph>
2026-08-25 00:25:02 +0800

    Update database connection for TiDB CLoud

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,16 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: 4000,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+
     waitForConnections: true,
     connectionLimit: 10
 });
