04ed225a35ea4f94896c5f5e202627ae4cbea351
diVelarde <divelarde@gbox.adnu.edu.ph>
2026-09-23 01:44:47 +0800

    [BLG-006] - Blog Role Restriction

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { requireAuth } = require('../middleware/authMiddleware');
 const { validateBlogPost } = require('../validators/blogPostValidators');
 const controller = require('../controllers/blogPostController');
+const { requireRole } = require('../middleware/roleMiddleware');
 
 const router = express.Router();
 
 router.get('/', controller.listPosts);
 router.get('/mine', requireAuth, controller.listMine);
 router.get('/:id', controller.getPostById);
-router.post('/', requireAuth, validateBlogPost, controller.createPost);
+router.post('/', requireAuth, requireRole('contributor', 'validator', 'admin'), validateBlogPost, controller.createPost);
 
 module.exports = router;
